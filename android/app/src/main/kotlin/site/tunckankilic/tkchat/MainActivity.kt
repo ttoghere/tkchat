3ae13509e5bf3df5983e999fc33141ae79833b91
ttoghere <tuncsoftwarehere@gmail.com>
2023-03-30 16:17:51 +0300

    First Setup is done
    This is a Professional Real time chat app with advanced code style i will add all of the details in read.me and taking good comments in project

---
+++ android/app/src/main/kotlin/site/tunckankilic/tkchat/MainActivity.kt
@@ -0,0 +1,6 @@
+package site.tunckankilic.tkchat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/common/middlewares/router_welcome.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tkchat/common/routes/routes.dart';
+import 'package:tkchat/common/store/store.dart';
+
+/// 第一次欢迎页面
+class RouteWelcomeMiddleware extends GetMiddleware {
+  // priority 数字小优先级高
+  @override
+  int? priority = 0;
+
+  RouteWelcomeMiddleware({required this.priority});
+
+  @override
+  RouteSettings? redirect(String? route) {
+    print(ConfigStore.to.isFirstOpen);
+    if (ConfigStore.to.isFirstOpen == false) {
+      return null;
+    } else if (UserStore.to.isLogin == true) {
+      return RouteSettings(name: AppRoutes.Application);
+    } else {
+      return RouteSettings(name: AppRoutes.SIGN_IN);
+    }
+  }
+}
